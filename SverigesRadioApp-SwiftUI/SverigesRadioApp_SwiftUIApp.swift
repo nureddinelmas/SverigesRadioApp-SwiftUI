9e585da57ba8bf9d5765df1acb17a5d635f8e4d8
nureddinelmas <nureddinelmas34@gmail.com>
2022-01-12 12:22:14 +0100

    Initial Commit

---
+++ SverigesRadioApp-SwiftUI/SverigesRadioApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SverigesRadioApp_SwiftUIApp.swift
+//  SverigesRadioApp-SwiftUI
+//
+//  Created by Nureddin Elmas on 2022-01-12.
+//
+
+import SwiftUI
+
+@main
+struct SverigesRadioApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
